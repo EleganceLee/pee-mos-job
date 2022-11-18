da117883c997105b146d1a9fe13289045e45f5cc
EleganceLee <elegance.mdev02@gmail.com>
2022-11-18 23:11:55 +0700

    done

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// const Infographic = document.getElementById("Infographic");
+// const Motion = document.getElementById("Motion");
+// const Interactive = document.getElementById("Interactive");
+
+// const card_item_int1 = document.getElementById("card-item-int1");
+// const card_item_int2 = document.getElementById("card-item-int2");
+// const card_item_int3 = document.getElementById("card-item-int3");
+
+// Infographic.addEventListener("click", () => {
+//     card_item_int1.style.display = "inline";
+//     card_item_int2.style.display = "none";
+//     card_item_int3.style.display = "none";
+// })
+
+// Motion.addEventListener("click", () => {
+//     card_item_int1.style.display = "none";
+//     card_item_int2.style.display = "block";
+//     card_item_int3.style.display = "none";
+// })
+
+// Interactive.addEventListener("click", () => {
+//     card_item_int1.style.display = "none";
+//     card_item_int2.style.display = "none";
+//     card_item_int3.style.display = "block";
+// })
